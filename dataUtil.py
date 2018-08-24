8663a3ac53a2aa57b52ec41774963969bfedd022
Mehul Shah <mehulmshah22@gmail.com>
2018-08-23 20:34:16 -0700

    train tf model to learn intent

--- dataUtil.py
+++ dataUtil.py
@@ -7,26 +7,31 @@
 
 import praw
 
-def getTrainData():
-    # init variables for text file, and Reddit object
-    # PRAW docs: https://praw.readthedocs.io/en/latest/code_overview/reddit_instance.html
+redditObj = praw.Reddit(client_id='9wHjRUw5P54JpA', \
+                     client_secret='SYx98S03esOePq05LGLwcLcxf50', \
+                     user_agent='financeScraper', \
+                     username='financeScraperBot', \
+                     password='botSCRAPERfinance')
+
+def getBalanceData():
     balanceFileName = 'checkBalanceData.txt'
-    housingDict = {'flair':'housing','queries':['afford house', 'can I buy a house']}
-    budgetingDict = {'flair':'budgeting','queries':['what is my budget', 'how much can I budget']}
-    redditObj = praw.Reddit(client_id='9wHjRUw5P54JpA', \
-                         client_secret='SYx98S03esOePq05LGLwcLcxf50', \
-                         user_agent='financeScraper', \
-                         username='financeScraperBot', \
-                         password='botSCRAPERfinance')
-    # Three lists of data points, one for each category. A data point is a String
-    # representing a user request
-    housingData = getRedditData(redditObj, housingDict['flair'], housingDict['queries'])
-    budgetingData = getRedditData(redditObj, budgetingDict['flair'], budgetingDict['queries'])
     with open(balanceFileName) as f:
         balanceData = [line.rstrip('\n') for line in f]
 
-    return balanceData + budgetingData[:100] + housingData[:100]
+    return balanceData
 
+def getBudgetingData():
+    budgetingDict = {'flair':'budgeting','queries':['what is my budget', 'how much can I budget']}
+    budgetingData = getRedditData(redditObj, budgetingDict['flair'], budgetingDict['queries'])
+    return budgetingData
+
+def getHousingData():
+    housingDict = {'flair':'housing','queries':['afford house', 'can I buy a house']}
+    housingData = getRedditData(redditObj, housingDict['flair'], housingDict['queries'])
+    return housingData
+
+def getTrainData():
+    return getBalanceData() + getHousingData() + getBudgetingData()
 
 def getRedditData(redditObj, flair, queryList):
     titles = []
