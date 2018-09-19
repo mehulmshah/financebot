66bf28dad86d48c0499f0271f524708dfcfd28d9
Mehul Shah <mehulmshah22@gmail.com>
2018-09-18 23:06:01 -0700

    comit

--- src/trainModel.py
+++ src/trainModel.py
@@ -7,7 +7,10 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
-from src.util.dataUtil import getBalanceData, getBudgetingData, getHousingData
+from src.util.dataUtil import getBalanceData, getBudgetingData, getHousingData, getNegExampleData
+from nltk.corpus import stopwords
+from sklearn import model_selection
+from sklearn import cross_validation
 import numpy as np
 import tflearn
 import tensorflow as tf
@@ -15,67 +18,58 @@ import random
 import json
 import pickle
 
+stemmer = LancasterStemmer()
+st = StanfordNERTagger('ner/english.muc.7class.distsim.crf.ser.gz', 'ner/stanford-ner.jar', encoding='utf-8')
 
-with open('src/data/conversation.json') as f:
+with open('src/data/intents.json') as f:
     intents = json.load(f)
 
-intents['categorySet'][0]['trainData'] += getBalanceData()
-intents['categorySet'][1]['trainData'] += getBudgetingData()
-intents['categorySet'][2]['trainData'] += getHousingData()
+intents['categorySet'][0]['trainData'] = getBalanceData()
+intents['categorySet'][1]['trainData'] = getBudgetingData()
+intents['categorySet'][2]['trainData'] = getHousingData()
+intents['categorySet'][3]['trainData'] = getNegExampleData()
 
 words = []
-categories = []
 sentences = []
+categories = ['balance', 'budgeting', 'housing', 'unknown']
 
-print('creating set of sentences, words, and categories...')
-for topic in intents['categorySet']:
-    for datapoint in topic['trainData']:
-        w = nltk.word_tokenize(datapoint)
-        words.extend(w)
-        sentences.append((w, topic['category']))
-        if topic['category'] not in categories:
-            categories.append(topic['category'])
+for category in intents['categorySet']:
+    for sentence in category['trainData']:
+        tokenized_sentence = word_tokenize(sentence)
+        words.extend(tokenized_sentence)
+        sentences.append((tokenized_sentence, category['name']))
 
-stemmer = LancasterStemmer()
-words = [stemmer.stem(w.lower()) for w in words if w.isalnum()]
+stop_words = set(stopwords.words('english'))
+words = [stemmer.stem(w.lower()) for w in words if w.isalnum() and not w in stop_words]
 words = list(dict.fromkeys(words))
 
-st = StanfordNERTagger('../stanford-ner/classifiers/english.muc.7class.distsim.crf.ser.gz',
-					   '../stanford-ner/stanford-ner.jar',
-					   encoding='utf-8')
-
-text = 'What is my Chase bank account balance if 3% is $300M?'
-classified_text = st.tag(word_tokenize(text))
-print(classified_text)
-
-
-print('creating training set...')
 trainingSet = []
 for sentence in sentences:
     bagOfWords = []
-    tokenizedWords = sentence[0]
-    tokenizedWords = [stemmer.stem(word.lower()) for word in tokenizedWords]
+    tokenized_sentence = sentence[0]
+    tokenized_sentence = [stemmer.stem(word.lower()) for word in tokenized_sentence]
     for w in words:
-        bagOfWords.append(1) if w in tokenizedWords else bagOfWords.append(0)
-    output = [0 for i in range(len(categories))]
+        bagOfWords.append(1) if w in tokenized_sentence else bagOfWords.append(0)
+    output = [0,0,0,0]
     output[categories.index(sentence[1])] = 1
     trainingSet.append([bagOfWords, output])
 
+classified_text = st.tag(word_tokenize(text))
+print(classified_text)
+
 random.shuffle(trainingSet)
 trainingSet = np.array(trainingSet)
-train_x = list(trainingSet[:,0])
-train_y = list(trainingSet[:,1])
+dataTrain, dataTest, labelTrain, labelTest = model_selection.train_test_split(trainingSet[:,0], trainingSet[:,1], test_size=0.25)
+dataTest, dataVal, labelTest, labelVal = cross_validation.train_test_split(dataTest, labelTest, test_size=0.5)
 
-print('creating neural net w/ tensorflow...')
 tf.reset_default_graph()
-net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.input_data(shape=[None, len(dataTrain[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.dropout(net, 0.5)
-net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.fully_connected(net, len(labelTrain[0]), activation='softmax')
 net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
 model = tflearn.DNN(net, tensorboard_dir='logs')
 
-print('fitting model to training data...')
-model.fit(train_x, train_y, n_epoch=100, batch_size=8, show_metric=True)
+model.fit(dataTrain, labelTrain, validation_set=(dataVal, labelVal), n_epoch=100, batch_size=8, show_metric=True, validation_batch_size=8)
 model.save('logs/model')
 pickle.dump({'words':words, 'categories':categories, 'train_x':train_x, 'train_y':train_y},open('src/data/training_data','wb'))
--- src/util/dataUtil.py
+++ src/util/dataUtil.py
@@ -15,61 +15,57 @@ redditObj = praw.Reddit(client_id='9wHjRUw5P54JpA', \
                      username='financeScraperBot', \
                      password='botSCRAPERfinance')
 
-# dict containing params for reddit searches
-queryDict = {'budgetingDict': {'flair':'budgeting','queries':['what should my budget look like?', 'how much can I budget']},
-             'housingDict': {'flair':'housing','queries':['afford house', 'can I buy a home']},
-             'entityDict': {'queries':['boa', 'Chase']}
-            }
-
-# path to manually annotated datasets
-BALANCE_DATA_PATH = 'src/data/checkBalanceData.txt'
-MORTGAGE_FAQ_PATH = 'src/data/mortgageFaqData.txt'
-
-# read in created dataset for training data for balance category
-def getBalanceData():
-    with open(BALANCE_DATA_PATH) as f:
-        balanceData = [line.rstrip('\n') for line in f]
-    return balanceData
-
-# scrape /r/personalfinance for posts about budgeting
-def getBudgetingData():
-    budgetingData = getRedditData(queryDict['budgetingDict']['flair'], queryDict['budgetingDict']['queries'])
-    for post in budgetingData:
-        if 'budget' not in post and 'budgeting' not in post:
-            budgetingData.remove(post)
-    return budgetingData[:50]
-
-# scrape /r/personalfinance for posts about housing affordability
-def getHousingData():
-    housingData = getRedditData(queryDict['housingDict']['flair'], queryDict['housingDict']['queries'])
-    return housingData[:50]
-
-def getMortgageFaqData():
-    with open(MORTGAGE_FAQ_PATH) as f:
-        mortgageFaqData = [line.rstrip('\n') for line in f]
-    return mortgageFaqData
-
 # helper method to actually scrape from Reddit
 def getRedditData(flair, queryList):
     titles = []
     for query in queryList:
         for post in redditObj.subreddit('personalfinance').search('flair:"{}" {}'.format(flair, query),limit=None):
             titles.append(post.title)
-
     return titles
 
-# get data for training NER model to recognize banks
+# dict containing params for reddit searches
+queryDict = {'budgetingDict': {'flair':'budgeting','queries':['what should my budget look like?', 'how much can I budget']},
+             'housingDict': {'flair':'housing','queries':['afford house']},
+             'entityDict': {'queries':['boa', 'Chase']},
+             'negExampleDict': {'flair':'auto','queries':['can I afford']}
+            }
+
+# path to manually annotated datasets
+BALANCE_DATA_PATH = 'src/data/checkBalanceData.txt'
+MORTGAGE_FAQ_PATH = 'src/data/mortgageFaqData.txt'
+SPENDING_DATA_PATH = 'src/data/spendingData.txt'
+NEG_DATA_PATH = 'src/data/negData.txt'
+
+def getNegExampleData():
+    autoData = getRedditData(queryDict['negExampleDict']['flair'], queryDict['negExampleDict']['queries'])
+    with open(NEG_DATA_PATH) as f:
+        otherNegData = [line.rstrip('\n') for line in f]
+    return autoData + otherNegData
+
+def getBalanceData():
+    with open(BALANCE_DATA_PATH) as f:
+        balanceData = [line.rstrip('\n') for line in f]
+    return balanceData
+
+def getBudgetingData():
+    budgetingData = getRedditData(queryDict['budgetingDict']['flair'], queryDict['budgetingDict']['queries'])
+    for post in budgetingData:
+        if 'budget' not in post and 'budgeting' not in post:
+            budgetingData.remove(post)
+    with open(SPENDING_DATA_PATH) as f:
+        spendingData = [line.rstrip('\n') for line in f]
+    return budgetingData + spendingData
+
+def getHousingData():
+    housingData = getRedditData(queryDict['housingDict']['flair'], queryDict['housingDict']['queries'])
+    with open(MORTGAGE_FAQ_PATH) as f:
+        mortgageFaqData = [line.rstrip('\n') for line in f]
+    return housingData + mortgageFaqData
+
 def getEntityData(limit):
     titles = []
     queryList = queryDict['entityDict']['queries']
     for query in queryList:
         for post in redditObj.subreddit('personalfinance').search('{}'.format(query),limit=limit):
             titles.append(post.title)
-    return titles
-
-# get data for training NER model to recognize prices in queries
-def getPriceData(limit):
-    titles = []
-    for post in redditObj.subreddit('budgetnerds').hot(limit=limit):
-        titles.append(post.title)
     return titles
